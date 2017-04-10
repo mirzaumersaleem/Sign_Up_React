131fe6d81fb374690cabdd33421f880357813daa
mirzaumersaleem <mirzaumersaleem.com>
2017-04-10 11:38:44 +0500

    Validations

--- src/signup.js
+++ src/signup.js
@@ -1,7 +1,11 @@
+//Form Validation Link :https://html5hive.org/reactjs-form-validation-tutorial/
+//bootstrap Link :https://react-bootstrap.github.io/components.html#forms
 import React,{Component} from 'react';
 import  './SignUp_css.css';
 import TableView from './TableView';
-
+import { Button } from 'react-bootstrap';
+import InputGroup from 'react-bootstrap';
+import FormControl from 'react-bootstrap';
 class SignUp extends Component{
 constructor(){
   super();
@@ -13,7 +17,8 @@ constructor(){
         password:'',
         cPassword:''
     },
-    fields:[]
+    fields:[],
+    error:'this is error'
   }
 }
 
@@ -49,24 +54,42 @@ render(){
 return(
    <div>
       <form className="signUp_css">  
+
             <br></br><br></br>
             <label><b>First Name</b></label>
-            <input className="inputField" type="text" value={this.state.myInfo.fName} onChange={this.inputChange.bind(this,"fName")}/>
+            <input className="inputField" type="text" value={this.state.myInfo.fName} onChange={this.inputChange.bind(this,"fName")}
+            required={true}
+            minLength={6}
+           maxLength={10}      
+            />
             <br></br><br></br>
             <label><b> Last Name</b></label>
-            <input type="text" value={this.state.myInfo.lName} onChange={this.inputChange.bind(this,"lName")}/>
+            <input type="text" value={this.state.myInfo.lName} onChange={this.inputChange.bind(this,"lName")}
+            required={true}
+            minLength={6}
+            maxLength={10} 
+            />
             <br></br><br></br>
             <label><b>E-mail</b></label>
-            <input type="text" value={this.state.myInfo.mobileNo} onChange={this.inputChange.bind(this,"e_mail")}/>
+            <input type="email" value={this.state.myInfo.mobileNo} onChange={this.inputChange.bind(this,"e_mail")}/>
+            <p color='white'>{this.state.error}</p>
             <br></br><br></br>
             <label><b>New password</b></label>
-            <input type="password" value={this.state.myInfo.password} onChange={this.inputChange.bind(this,"password")}/>
+            <input type="password" value={this.state.myInfo.password} onChange={this.inputChange.bind(this,"password")} 
+            required={true}
+            minLength={6}
+           maxLength={10} 
+            />
             <br></br><br></br>
             <label><b>Confirm New Password</b></label>
-            <input type="password" value={this.state.cPassword} onChange={this.inputChange.bind(this,"cPassword")}/>
+            <input type="password" value={this.state.cPassword} onChange={this.inputChange.bind(this,"cPassword")}
+            required={true}
+            minLength={6}
+           maxLength={10} 
+            />
             <br></br><br></br>
           
-            <button onClick={this.successMessage.bind(this)}><b>Create Account</b></button>
+            <Button bsStyle="primary" onClick={this.successMessage.bind(this)}><b>Create Account</b></Button>
        </form>
 
         <div>
