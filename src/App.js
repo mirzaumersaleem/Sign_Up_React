12ec7b2b947f6ee27268840c33513c96dcfd9321
mirzaumersaleem <mirzaumersaleem.com>
2017-05-12 21:14:51 +0500

    Login Form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Inside View</h2>
+          <h2>Attribe Solution E-Banking</h2>
         </div>
         
         <SignUp/>
--- src/signup.js
+++ src/signup.js
@@ -2,10 +2,10 @@
 //bootstrap Link :https://react-bootstrap.github.io/components.html#forms
 import React,{Component} from 'react';
 import  './SignUp_css.css';
-import TableView from './TableView';
+
 import { Button } from 'react-bootstrap';
-import InputGroup from 'react-bootstrap';
-import FormControl from 'react-bootstrap';
+// import InputGroup from 'react-bootstrap';
+// import FormControl from 'react-bootstrap';
 class SignUp extends Component{
 constructor(){
   super();
@@ -30,21 +30,11 @@ successMessage(event){
     })
     event.preventDefault();
 }
-//change Value mie fields arhi hain 
-//event mie value arhi hai 
 
 inputChange(changeValue,event){
-    // var temp = {};
-    // temp[changeValue] = event.target.value;
-    // this.setState(temp);
-
-    // changeValue can contain any key in the myInfo object. like firstName lname,
-    // we are assigning value to that key.
-
+ 
     this.state.myInfo[changeValue] = event.target.value;
-
-    // update state
-    this.setState({
+this.setState({
         myInfo : this.state.myInfo
     });
 
@@ -55,15 +45,15 @@ return(
    <div>
       <form className="signUp_css">  
 
-            <br></br><br></br>
-            <label><b>First Name</b></label>
+            <br></br>
+            <label><b>User Name</b></label>
             <input className="inputField" type="text" value={this.state.myInfo.fName} onChange={this.inputChange.bind(this,"fName")}
             required={true}
-            minLength={6}
-           maxLength={10}      
+            minLength={4}
+            maxLength={20}      
             />
-            <br></br><br></br>
-            <label><b> Last Name</b></label>
+            <br></br>
+            {/*<label><b> Last Name</b></label>
             <input type="text" value={this.state.myInfo.lName} onChange={this.inputChange.bind(this,"lName")}
             required={true}
             minLength={6}
@@ -72,29 +62,27 @@ return(
             <br></br><br></br>
             <label><b>E-mail</b></label>
             <input type="email" value={this.state.myInfo.mobileNo} onChange={this.inputChange.bind(this,"e_mail")}/>
-            <p color='white'>{this.state.error}</p>
-            <br></br><br></br>
-            <label><b>New password</b></label>
+            <p color='white'>{this.state.error}</p>*/}
+            <br></br>
+            <label><b>Password</b></label>
             <input type="password" value={this.state.myInfo.password} onChange={this.inputChange.bind(this,"password")} 
             required={true}
             minLength={6}
-           maxLength={10} 
+           maxLength={18} 
             />
-            <br></br><br></br>
-            <label><b>Confirm New Password</b></label>
+            <br></br>
+            {/*<label><b>Confirm New Password</b></label>
             <input type="password" value={this.state.cPassword} onChange={this.inputChange.bind(this,"cPassword")}
             required={true}
             minLength={6}
            maxLength={10} 
-            />
-            <br></br><br></br>
+            />*/}
+            <br></br>
           
-            <Button bsStyle="primary" onClick={this.successMessage.bind(this)}><b>Create Account</b></Button>
+            <Button bsStyle="primary" onClick={this.successMessage.bind(this)}><b>Login</b></Button>
        </form>
 
-        <div>
-        <TableView fields={this.state.fields}/>
-        </div>
+      
 
    </div>
 )
